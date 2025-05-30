4a9dcea69aaa18100b59b72cd365a95b70903c4b
Ali <m.ali.rezaeiii09@gmail.com>
2025-05-31 02:54:22 +0330

    Cleanup

--- composeApp/src/androidMain/kotlin/iq/tiptapp/MyActivityHolder.kt
+++
@@ -1,9 +0,0 @@
-package iq.tiptapp
-
-import android.annotation.SuppressLint
-import android.app.Activity
-
-@SuppressLint("StaticFieldLeak")
-object MyActivityHolder {
-    lateinit var activity: Activity
-}
