957e3e16a8cef72209a228d3b7b416ad337e7b92
Ali <m.ali.rezaeiii09@gmail.com>
2025-07-07 19:42:45 +0330

    Using SaveStateHandle in case of process death

--- composeApp/src/commonMain/kotlin/iq/tiptapp/ui/help/HelpViewModel.kt
+++ composeApp/src/commonMain/kotlin/iq/tiptapp/ui/help/HelpViewModel.kt
@@ -1,21 +1,38 @@
 package iq.tiptapp.ui.help
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class HelpViewModel : ViewModel() {
-    private val _pickUpClickedLocation = MutableStateFlow<Pair<Double, Double>?>(null)
+class HelpViewModel(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _pickUpClickedLocation = MutableStateFlow(
+        savedStateHandle.get<Pair<Double, Double>?>(KEY_PICK_UP)
+    )
     val pickUpClickedLocation: StateFlow<Pair<Double, Double>?> = _pickUpClickedLocation
 
-    private val _dropOffClickedLocation = MutableStateFlow<Pair<Double, Double>?>(null)
+    private val _dropOffClickedLocation = MutableStateFlow(
+        savedStateHandle.get<Pair<Double, Double>?>(KEY_DROP_OFF)
+    )
     val dropOffClickedLocation: StateFlow<Pair<Double, Double>?> = _dropOffClickedLocation
 
     fun onMarkerPickUpClicked(lat: Double, lng: Double) {
-        _pickUpClickedLocation.value = lat to lng
+        val location = lat to lng
+        _pickUpClickedLocation.value = location
+        savedStateHandle[KEY_PICK_UP] = location
     }
 
     fun onMarkerDropOffClicked(lat: Double, lng: Double) {
-        _dropOffClickedLocation.value = lat to lng
+        val location = lat to lng
+        _dropOffClickedLocation.value = location
+        savedStateHandle[KEY_DROP_OFF] = location
+    }
+
+    companion object {
+        private const val KEY_PICK_UP = "pick_up_location"
+        private const val KEY_DROP_OFF = "drop_off_location"
     }
 }
