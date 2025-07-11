eb368af3453c4c2da617681c2bbf95d9e8d382fb
Ali <m.ali.rezaeiii09@gmail.com>
2025-07-11 20:23:00 +0330

    Rename

--- composeApp/src/iosMain/kotlin/iq/tiptapp/di/iOSKoin.kt
+++ composeApp/src/iosMain/kotlin/iq/tiptapp/di/iOSKoin.kt
@@ -2,10 +2,11 @@ package iq.tiptapp.di
 
 import iq.tiptapp.help.HelpViewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 fun getIosModules(): List<Module> = listOf(iosViewModelModule)
 
 val iosViewModelModule = module {
-    factory { HelpViewModel() }
+    factoryOf(::HelpViewModel)
 }
